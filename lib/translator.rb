c33e3e4cfbd1fa08f5474ee982a450e36661e2fa
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-16 12:44:10 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,9 +1,14 @@
-# require modules here
-
-def load_library
-  # code goes here
+  
+def load_library(file)
+  require 'yaml'
+  stuff=YAML.load_file(file)
+  newhash={"get_meaning"=> {}, "get_emoticon" => {}}
+  stuff.each do|meaning,emoticons|
+    newhash["get_meaning"][emoticons[1]]=meaning
+    newhash["get_emoticon"][emoticons[0]]=meaning
+  end
+  newhash
 end
-
 def get_japanese_emoticon
   # code goes here
 end
@@ -11,3 +16,5 @@ end
 def get_english_meaning
   # code goes here
 end
+
+puts load_library("./lib/emoticons.yml")
